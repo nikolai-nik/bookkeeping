00ab0b5c5b7b2289f4665da909c2d5c55fb54d67
Nikolai Voytsekhovskyy <nikolai.vo@dreamscraft.tech>
2019-01-16 12:50:34 +0200

    Implement reports-page

--- src/app/components/reports/reports.component.ts
+++ src/app/components/reports/reports.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import { SendService } from 'src/app/shared/services/send.service';
+import { switchMap, map } from 'rxjs/operators';
 @Component({
   selector: 'app-reports',
   templateUrl: './reports.component.html',
   styleUrls: ['./reports.component.scss']
 })
 export class ReportsComponent implements OnInit {
+  public reportSection: any;
+  public resIncome: any;
+  constructor(private sendService: SendService) {
+    // this.reportSection = this.sendService.getToDatabase('income').subscribe((res) => {
+    //   return res;
+    // });
+    // this.sendService.filterBy('income')
+    // console.log( this.reportSection)
+    this.sendService.getSectionDate('income').subscribe((res) => {
+      this.resIncome = res.filter((item: any): any => item.data.taxesSelect == 5);
+       console.log(this.resIncome)
+    })
+   
 
-  constructor() { }
+    this.sendService.filterSec('income').subscribe(res => console.log(res))
+  }
 
   ngOnInit() {
   }
-
+  onClickFilter() {
+   console.log(this.resIncome)
+  }
 }
--- src/app/shared/services/send.service.ts
+++ src/app/shared/services/send.service.ts
@@ -1,11 +1,54 @@
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase } from '@angular/fire/database';
-
+import { AngularFireDatabase, AngularFireAction, AngularFireList } from '@angular/fire/database';
+import { Observable, Subscription, BehaviorSubject, pipe, forkJoin } from 'rxjs';
+import { switchMap, map } from 'rxjs/operators';
 @Injectable()
 export class SendService {
-    constructor(private af: AngularFireDatabase) { }
-    public SendToDatabase(section: string, data) {
-        return this.af.list(`sections/${section}`).push(data);
-    }
+  public items$: Observable<any[]>
+  public size$: BehaviorSubject<string | null>;
 
+  public items: Observable<any[]>;
+  constructor(private af: AngularFireDatabase) {
+    this.size$ = new BehaviorSubject(null);
+    
+  
+   
+  }
+
+  public getSectionDate(section: string) {
+  
+     return this.af.list(`sections/income`).snapshotChanges().pipe(
+        map(changes =>
+          changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+        )
+      )
+    
+    
+  }
+
+  public filterSec(section: string) {
+    return this.af.list(`sections/${section}`, ref => ref.orderByChild('data/name').equalTo('test')).snapshotChanges()
+  }
+  public GetSectionDatabase(section: string) {
+    return this.af.list(`sections/${section}`);
+  }
+  public SendToDatabase(section: string, data) {
+    return this.af.list(`sections/${section}`).push(data);
+  }
+  // public getToDatabase(section: string) {
+  //   return this.items$ = this.size$.pipe(
+  //     switchMap(size =>
+  //       this.af.list(`sections/${section}`, ref =>
+  //         size ? ref.orderByChild('size').equalTo(size) : ref
+  //       ).snapshotChanges().pipe(
+  //         map(changes =>
+  //           changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+  //         )
+  //       )
+  //     )
+  //   );
+  // }
+  // public filterBy(period: string | null) {
+  //   this.size$.next(period);
+  // }
 }
